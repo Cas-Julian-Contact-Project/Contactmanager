3a7cb9991a849a742e1a8aae6eee9b4b32ba5c22
Casanova Geary <CasanovaGeary@Gmail.com>
2022-10-14 12:25:05 -0500

    Array List not working well, Break.

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hello
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        String firstName;
+        firstName = "Cas";
+        String lastName;
+        lastName = "Ugly";
+        long phoneNumber;
+        phoneNumber = 8675309;
+        String formatContacts;
+        formatContacts = "";
 
+        Contact contact = new Contact(firstName, lastName, phoneNumber);
+
+        formatContacts = contact.getFirstName() + " " + contact.getLastName() + " | " + contact.getPhoneNumber();
+        ArrayList<String> contactInfo = new ArrayList<>();
+        contactInfo.add(formatContacts);
+//        ArrayList<String> help = new ArrayList<>();
+//        help.add(formatContacts);
+//
+//        System.out.println(help);
+//
+        for(String entry : contactInfo)
+        {
+            System.out.println(entry);
+        }
     }
 }
