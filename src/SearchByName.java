6006a6efd91fd7dbc026d1c1f7a05bae122dfa89
Casanova Geary <CasanovaGeary@Gmail.com>
2022-10-18 12:29:53 -0500

    Working on user input results.

--- src/SearchByName.java
+++ src/SearchByName.java
@@ -7,14 +7,14 @@ public class SearchByName
     {
         Scanner scanner = new Scanner(System.in);
         //create a boolean variable named isNameFound and assign it to true;
-        boolean isNameFound = true;
+        String isNameFound = "false";
 
         //create a String variable and name it searchField and assign the input of the user to this variable
         String searchField;
 
         //prompt the user to search by first or last name
         System.out.println("\nSearch by First and/or Last name: ");
-        searchField = scanner.next();
+        searchField = scanner.nextLine().toLowerCase();
 
         //loop through the searchName list and check if the searchField matches either the first name or the last name field
         for(String entry : searchName)
@@ -22,24 +22,46 @@ public class SearchByName
             //split the list and assign it to firstLastName array
             String[] firstLastName = entry.split(" ");
 
+            String firstName = firstLastName[0].toLowerCase();
+            String lastName = firstLastName[1].toLowerCase();
+            String firstLetterFirstName = String.valueOf(firstName.charAt(0));
+            String firstLetterLastName = String.valueOf(lastName.charAt(0));
+            String lettersInSequence = firstName.substring(0,3);
+            String lettersInSequenceToo = lastName.substring(0,3);
+
             //if the searchField matches either the first or last name, display the name(s) to the user
-            if(firstLastName[0].equalsIgnoreCase(searchField) || firstLastName[1].equalsIgnoreCase(searchField))
+            if(firstLetterFirstName.equalsIgnoreCase(searchField) || firstLetterLastName.equalsIgnoreCase(searchField) || lettersInSequence.equalsIgnoreCase(searchField) || lettersInSequenceToo.equalsIgnoreCase(searchField) || firstName.equalsIgnoreCase(searchField) || lastName.equalsIgnoreCase(searchField))
             {
-                System.out.println();
                 System.out.println(entry);
-                //assign true to isNameFound
-                isNameFound = true;
-                break;
-            }
-            //if there are no names that match assign false to isNameFound
-            else {
-                isNameFound = false;
+                isNameFound = "true";
             }
         }
+
         //if isNameFound false, display a message to the user letting them know that the name is not in the directory
-        if(!isNameFound)
+        if(isNameFound.equals("false"))
         {
-            System.out.printf("%nYour input %s is not in directory.%n", searchField);
+            System.out.printf("%nYour input %s is not in file.%n", searchField);
         }
+
+        do
+        {
+            System.out.println("Would you like to search again? Yes or No?");
+            searchField = scanner.nextLine();
+
+            if (searchField.equalsIgnoreCase("Yes") || (searchField.equalsIgnoreCase("Y")))
+            {
+                searchByName(searchName);
+//            } else if (searchField.equalsIgnoreCase("No") || searchField.equalsIgnoreCase("N"))
+//            {
+////                break;
+            }
+            else if(!searchField.equalsIgnoreCase("Yes") || (!searchField.equalsIgnoreCase("Y")) || !searchField.equalsIgnoreCase("No") || !searchField.equalsIgnoreCase("N"))
+            {
+                System.out.println("Please enter Yes or No?");
+//                System.out.println("Would you like to search again? Yes or No?");
+//                searchField = scanner.nextLine();
+            }
+        } while(searchField.equalsIgnoreCase("No") || (searchField.equalsIgnoreCase("N")));
+
     }
 }
